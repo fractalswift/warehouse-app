a9c2abaecc6de799a73bac1fa5113d194e010225
fractalswift <matthewthomasmacklin@gmail.com>
2020-02-09 14:57:56 +0000

    Updates

---
+++ precache-manifest.8fde64e6c2e882e7459d5e4288437c28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715b527b7874835dd45299a859d8f438",
+    "url": "/warehouse-app/index.html"
+  },
+  {
+    "revision": "cba481d2a4822219d4ab",
+    "url": "/warehouse-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ec20578cd388694cb329",
+    "url": "/warehouse-app/static/js/2.c4239c01.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/warehouse-app/static/js/2.c4239c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba481d2a4822219d4ab",
+    "url": "/warehouse-app/static/js/main.9fe954df.chunk.js"
+  },
+  {
+    "revision": "851475b1d7020fefec12",
+    "url": "/warehouse-app/static/js/runtime-main.f169f6a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/warehouse-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
